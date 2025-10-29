18b6d53358551179dc813fce8cfa3fdb622b625b
indkshitij <ind.kshitijsingh@gmail.com>
2025-10-29 19:58:47 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -20,16 +19,3 @@ createRoot(document.getElementById("root")).render(
     </ClerkProvider>
   </StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <App />
-  </StrictMode>,
-)
->>>>>>> 909e6715edd6db1879f6c9b410ad7d0f0116d5bb
